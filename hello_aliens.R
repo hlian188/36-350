2480cce6a3bda477ca793047b331867dfc7ba655
hlian188 <harrisonlian188@gmail.com>
2020-07-30 22:01:16 -0400

    Initial commit

---
+++ hello_aliens.R
@@ -0,0 +1,6 @@
+cat("Hi aliens, if you see this message, can you answer some questions I have \n
+    1. Did you build the Great Pyramids
+    2. Did you know that the History Channel has a show about you guys
+    3. Have you abducted any humans
+    4. Have you visited Earth?
+    5. What's at Area51")
